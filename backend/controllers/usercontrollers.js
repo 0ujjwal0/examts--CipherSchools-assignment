526d15afd3236d694a7ef93b4872f519f69c0bed
ujjwal <ujjwal.12100544@lpu.in>
2024-08-29 22:47:18 +0530

    bug fix

--- backend/controllers/usercontrollers.js
+++ backend/controllers/usercontrollers.js
@@ -5,7 +5,7 @@ const generateToken = require("../modal/config/generateToken");
 
 // Register a new user
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, pic } = req.body;
+  const { name, email, password } = req.body;
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please enter all fields");
